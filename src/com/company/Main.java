a8ca94bad1b88fefa10557a43cfdb7c497d8783e
Delirashka <102419248+Delirashka@users.noreply.github.com>
2022-03-26 17:00:53 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = new int[] {39,78,56,36,24,25,15,15,89,71};
+        bubbleSort(array);
+        System.out.println(arrayToString(array));
+    }
+
+    public static void bubbleSort(int[] array) {
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i - 1] > array[i]) {
+                    int tmp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = tmp;
+                    isSorted = false;
+                }
+            }
+        }
+    }
+
+    private static String arrayToString(int[] array) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < array.length; i++) {
+            if (i > 0) {
+                sb.append(", ");
+            }
+            sb.append(array[i]);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
